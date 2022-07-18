3deb5700e451d68744d8bf954425016e2efefb8d
Satrumidium <32928338+Satrumidium@users.noreply.github.com>
2022-07-18 23:49:06 +0200

    Completed timer functionality

--- components/TimerModule.tsx
+++ components/TimerModule.tsx
@@ -1,10 +1,77 @@
-import { Box } from "@mantine/core";
+/* eslint-disable react/jsx-no-bind */
 
-const TimerModule = (): JSX.Element => {
+import { Box, Button, MediaQuery, SegmentedControl } from "@mantine/core";
+import { Dispatch, SetStateAction, useState } from "react";
+// import logger from "../utils/logger";
+
+interface timerProps {
+  seconds: number;
+  minutes: number;
+  timeToggle: boolean;
+  setTimeToggle: Dispatch<SetStateAction<boolean>>;
+}
+
+function padZero(integer: number): string {
+  if (integer < 10) {
+    return `0${integer}`;
+  }
+  return integer.toString();
+}
+
+const TimerModule = ({
+  seconds,
+  minutes,
+  timeToggle,
+  setTimeToggle,
+}: timerProps): JSX.Element => {
+  const [segment, setSegment] = useState("timer");
+  const [segmentColour, setSegmentColour] = useState("blue");
+  const [startButton, setStartButton] = useState("Start");
+
+  function handleSegment(e: string) {
+    setSegment(e);
+    if (e === "timer") {
+      setSegmentColour("blue");
+    } else {
+      setSegmentColour("red");
+    }
+  }
+
+  function handleClick() {
+    setTimeToggle(!timeToggle);
+    if (startButton === "Start") {
+      setStartButton("Pause");
+    } else {
+      setStartButton("Start");
+    }
+  }
+
+  // logger.info(timerTime.getHours());
+
   return (
-    <Box>
-      <div>Timer</div>
-    </Box>
+    <MediaQuery smallerThan="sm" styles={{ width: "250px" }}>
+      <Box sx={{ margin: "auto", width: "45%" }}>
+        <SegmentedControl
+          fullWidth
+          radius="md"
+          size="lg"
+          value={segment}
+          onChange={handleSegment}
+          color={segmentColour}
+          transitionDuration={300}
+          data={[
+            { label: "Timer", value: "timer" },
+            { label: "Break", value: "break" },
+          ]}
+        />
+        <Box sx={{ backgroundColor: "red" }}>
+          {padZero(minutes)} {padZero(seconds)}
+        </Box>
+        <Button fullWidth onClick={handleClick}>
+          {startButton}
+        </Button>
+      </Box>
+    </MediaQuery>
   );
 };
 
--- pages/timer.tsx
+++ pages/timer.tsx
@@ -1,16 +1,45 @@
 import type { NextPage } from "next";
-import { AppShell } from "@mantine/core";
+import { AppShell, Stack } from "@mantine/core";
+import { useEffect, useState } from "react";
+import logger from "../utils/logger";
 
-import { useState } from "react";
-import Navigation from "../components/Navigation";
+import Navigation from "../components/AppShell/Navigation";
 import TimerModule from "../components/TimerModule";
 import CurrentTasks from "../components/CurrentTasks";
 import TaskLake from "../components/TaskLake";
-import FooterModule from "../components/FooterModule";
-import HeaderModule from "../components/HeaderModule";
+import FooterModule from "../components/AppShell/FooterModule";
+import HeaderModule from "../components/AppShell/HeaderModule";
 
 const TimePage: NextPage = () => {
   const [opened, setOpened] = useState(false);
+
+  // Only minutes and seconds are relevant
+  const [seconds, setSeconds] = useState(2);
+  const [minutes, setMinutes] = useState(1);
+  const [timeToggle, setTimeToggle] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    let timer: string | number | NodeJS.Timer;
+    if (timeToggle) {
+      timer = setInterval(() => {
+        if (seconds === 0 && minutes === 0) {
+          return;
+        }
+        if (seconds === 0) {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+          return;
+        }
+        setSeconds(seconds - 1);
+      }, 1000);
+    }
+
+    logger.info(seconds);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [minutes, seconds, timeToggle]);
 
   return (
     <AppShell
@@ -19,9 +48,17 @@ const TimePage: NextPage = () => {
       header={<HeaderModule opened={opened} setOpened={setOpened} />}
       navbar={<Navigation opened={opened} />}
     >
-      <TimerModule />
-      <CurrentTasks />
-      <TaskLake />
+      <Stack sx={{ textAlign: "center" }}>
+        <TimerModule
+          seconds={seconds}
+          minutes={minutes}
+          timeToggle={timeToggle}
+          setTimeToggle={setTimeToggle}
+        />
+
+        <CurrentTasks />
+        <TaskLake />
+      </Stack>
     </AppShell>
   );
 };
