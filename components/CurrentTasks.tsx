c90c061085512fdb68f34a4a38b8651110c72099
Satrumidium <32928338+Satrumidium@users.noreply.github.com>
2022-07-20 14:52:06 +0200

    Implemented delete and complete tasks

--- components/CurrentTasks.tsx
+++ components/CurrentTasks.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, MediaQuery } from "@mantine/core";
-import { Dispatch, SetStateAction } from "react";
+import { Box, Button, Checkbox, MediaQuery } from "@mantine/core";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
 import { TaskObj } from "../utils/types";
 import logger from "../utils/logger";
 
@@ -22,10 +22,36 @@ const CurrentTasks = ({
       id: new Date().getTime().toString(),
       endTime: new Date(),
       startTime: new Date(),
+      done: false,
     };
     const tempTasks = [...tasks, tempTask];
     setTasks(tempTasks);
     logger.info(tasks);
+  }
+
+  function completeTask(e: ChangeEvent<HTMLInputElement>) {
+    logger.info(e.target.id);
+    logger.info(e.target.checked);
+    const tempTasks = [...tasks];
+    tempTasks.forEach((task) => {
+      if (e.target.id === task.id && e.target.checked) {
+        task.done = true;
+      }
+      if (e.target.id === task.id && !e.target.checked) {
+        task.done = false;
+      }
+    });
+    setTasks(tempTasks);
+  }
+
+  function deleteTask(e: ChangeEvent<HTMLButtonElement>) {
+    const tempTasks = [...tasks];
+    tempTasks.forEach((task, index, array) => {
+      if (e.currentTarget.id === task.id) {
+        array.splice(index, 1);
+      }
+    });
+    setTasks(tempTasks);
   }
 
   // @ts-ignore
@@ -34,7 +60,21 @@ const CurrentTasks = ({
       <Box sx={{ margin: "auto", width: "40%", backgroundColor: "lightcoral" }}>
         <Box>
           {tasks.map((task) => {
-            return <div key={task.id}>{task.name}</div>;
+            let strike;
+            if (task.done) {
+              strike = "line-through";
+            }
+            return (
+              <Box key={task.id}>
+                <Box sx={{ textDecoration: strike }}>
+                  {task.name} with id: {task.id}
+                </Box>
+                <Checkbox id={task.id} onClick={completeTask} />
+                <Button id={task.id} onClick={deleteTask}>
+                  Delete Task
+                </Button>
+              </Box>
+            );
           })}
         </Box>
         <Button fullWidth onClick={createTask}>
