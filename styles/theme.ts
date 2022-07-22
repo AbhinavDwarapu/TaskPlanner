2c44f3941f3414111595ab6180999c2f86f1965c
Satrumidium <32928338+Satrumidium@users.noreply.github.com>
2022-07-22 22:29:26 +0200

    Added initial colours

--- styles/theme.ts
+++ styles/theme.ts
@@ -6,6 +6,33 @@ const theme = {
     lg: 1200,
     xl: 1400,
   },
+  colors: {
+    custom: [
+      "#d7f8ff",
+      "#88dbe7",
+      "#44CADC",
+      "#2AC9DE",
+      "#1AC2D9",
+      "#11B7CD",
+      "#09ADC3",
+      "#0E99AC",
+      "#128797",
+      "#147885",
+      "#f9fdff",
+    ],
+    custom2: [
+      "#F0BBDD",
+      "#ED9BCF",
+      "#EC7CC3",
+      "#ED5DB8",
+      "#F13EAF",
+      "#F71FA7",
+      "#ffaddf",
+      "#E00890",
+      "#C50E82",
+      "#AD1374",
+    ],
+  },
 };
 
 export default theme;
