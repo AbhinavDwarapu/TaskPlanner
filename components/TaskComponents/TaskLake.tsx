79e8469489fdbd414cc036aaa2068030a7239061
Satrumidium <32928338+Satrumidium@users.noreply.github.com>
2022-07-24 01:41:01 +0200

    Removed task lake until required

--- components/TaskComponents/TaskLake.tsx
+++ components/TaskComponents/TaskLake.tsx
@@ -1,11 +1,7 @@
 import { Box } from "@mantine/core";
 
 const TaskLake = (): JSX.Element => {
-  return (
-    <Box>
-      <div>Task Lake</div>
-    </Box>
-  );
+  return <Box>{/* <div>Task Lake</div> */}</Box>;
 };
 
 export default TaskLake;
