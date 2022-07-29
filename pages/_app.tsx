dd55ae0bf4a3ad62364518f205d9b12373d6b4fd
Satrumidium <32928338+Satrumidium@users.noreply.github.com>
2022-07-29 19:24:39 +0200

    Implemented notification settings function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,35 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
+import { useLocalStorage } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 import theme from "../styles/theme";
+import { SettingsFormObj } from "../utils/types";
+import logger from "../utils/logger";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [settings] = useLocalStorage<SettingsFormObj>({
+    key: "settings",
+    defaultValue: {
+      timerMinutes: 45,
+      timerSeconds: 0,
+      breakMinutes: 25,
+      breakSeconds: 0,
+      notifications: true,
+    },
+  });
+
+  const [limit, setLimit] = useState(3);
+
+  useEffect(() => {
+    if (settings.notifications) {
+      setLimit(3000);
+    } else {
+      setLimit(0);
+    }
+    logger.info(limit);
+  }, [limit, settings.notifications]);
+
   return (
     <>
       <Head>
@@ -20,7 +46,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
 
       <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
-        <NotificationsProvider limit={2}>
+        <NotificationsProvider autoClose={limit} position="bottom-center">
           <Component {...pageProps} />
         </NotificationsProvider>
       </MantineProvider>
