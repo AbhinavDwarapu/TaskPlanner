25c53a8cb909d4232103dff10d7ecabd7b022587
Satrumidium <32928338+Satrumidium@users.noreply.github.com>
2022-08-14 23:21:45 +0200

    Added CapacitorJS for mobile support

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.taskplanner.app",
+  appName: "Task Planner",
+  webDir: "out",
+  bundledWebRuntime: false,
+};
+
+export default config;
